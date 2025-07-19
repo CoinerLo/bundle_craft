55ee6d445f8df62d6dc27bfa1a2936fabf85647a
CoinerLo <coiner4677@gmail.com>
2025-07-19 16:36:44 +0300

    5 iteration 2 task

--- tasks/builder/bundle.js
+++ tasks/builder/bundle.js
@@ -17,17 +17,19 @@ import path from "node:path";
  * @param {string} entryPath - путь к entry бандлинга
  */
 export function bundle(entryPath) {
+  const Global_Exports = {};
   const filesData = [];
   const file = fs.readFileSync(path.resolve(entryPath), 'utf8');
+  Global_Exports[entryPath] = file;
   filesData.push(file);
   const pathList = searchRequireCalls(file);
   const getData = () => {
     if (pathList.length) {
       const nextFilePath = pathList.pop();
       const nextDirPath = path.dirname(entryPath);
-      console.log(nextFilePath, nextDirPath)
       const nextFile = fs.readFileSync(path.resolve(nextDirPath, nextFilePath), 'utf8');
       filesData.push(nextFile);
+      Global_Exports[nextFilePath] = nextFile;
 
       const nextPathList = searchRequireCalls(nextFile);
       if (nextPathList.length) {
@@ -38,8 +40,25 @@ export function bundle(entryPath) {
   }
 
   getData();
-  console.log(filesData)
-  return file.concat('\n');
+
+  const getRuntime = (data) => data.map((item) => {
+    return `
+  (function a(){
+    ${item.trim()}
+  })();`;
+  });
+  const allData = getRuntime(filesData).join('');
+  const prepareGlobalExports = Object.entries(Global_Exports).map(([key, val]) => `'${key}': () => {${val.replace('module.exports =', 'return')}}`)
+  const result = `var Global_Exports ={ ${prepareGlobalExports.join(',')} };
+if (typeof require === 'function') {
+  ${allData}
+} else {
+  var require = (arg) => {const result = Global_Exports[arg]();return result};
+  var module = { exports: {} };
+  ${allData}
+}`
+
+  return result;
 }
 
 /**
