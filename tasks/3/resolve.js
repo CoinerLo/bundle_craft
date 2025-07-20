811e930a694514d9dfd62cb9731a1985616fb84f
CoinerLo <coiner4677@gmail.com>
2025-07-20 11:30:28 +0300

    1 iteration 3 task

--- tasks/3/resolve.js
+++ tasks/3/resolve.js
@@ -9,7 +9,31 @@ const rootDir = path.resolve(".");
 const extensionsToResolve = ["js", "ts", "json"];
 
 export function resolve(importPath, parentPath) {
-  
+  const splitingImportPath = importPath.split('/');
+  if (splitingImportPath[0] === '..') {
+    const nextDirPath = path.dirname(parentPath);
+    const newPath = path.resolve(nextDirPath, importPath);
+    const fileName = splitingImportPath.pop();
+    const splitingFileName = fileName.split('.');
+    // console.log(newPath, importPath, parentPath)
+    if (splitingFileName.length < 2) {
+      let result = null;
+      const iter = [...extensionsToResolve];
+      while (result === null && iter.length > 0) {
+        const ext = iter.shift();
+        const next = newPath + '.' + ext;
+        result = isFileExists(next);
+        console.log(result, next)
+      }
+
+        console.log(result)
+      return result;
+    } else {
+      return isFileExists(newPath);
+    }
+  } else {
+    
+  }
 }
 
 function isFileExists(filePath) {
