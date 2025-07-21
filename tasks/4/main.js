8e9f50f5278b2250526c25214367f96f0ffa873b
CoinerLo <coiner4677@gmail.com>
2025-07-21 21:32:20 +0300

    1 iteration 4 task

--- tasks/4/main.js
+++ tasks/4/main.js
@@ -4,12 +4,15 @@ import * as astring from "astring";
 import { parse } from "acorn";
 
 // read file with source code
+const sourceCode = fs.readFileSync('./entry.js', 'utf-8');
 
 // get ast from source code
-
+const ast = parse(sourceCode, { ecmaVersion: 6 })
+console.log(ast);
 // transform ast
 
 // convert ast to source code
+const result = '';
 
 // write source code to file
 fs.writeFileSync("./result.js", result);
