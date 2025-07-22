6352d6ea9ab2575abd811351d24f9899b9d042f3
CoinerLo <coiner4677@gmail.com>
2025-07-22 20:53:57 +0300

    update task 1

--- tasks/1/build.sh
+++ tasks/1/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf dist && mkdir dist && cat ./src/jquery.js ./src/index.js | sed 's/  */ /g' | tr -d '\t\n' > dist/entry.js
+rm -rf dist && mkdir dist && cat ./src/jquery.js ./src/index.js | tr -d '\n\t' | tr -s ' ' > dist/entry.js
