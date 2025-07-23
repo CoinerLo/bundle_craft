4044f02ec95da78b211c2df513350075f5c8117f
CoinerLo <coiner4677@gmail.com>
2025-07-23 20:51:41 +0300

    4 iteration 4 task

--- tasks/4/transformer.js
+++ tasks/4/transformer.js
@@ -2,5 +2,15 @@ import { walk } from 'estree-walker'
 
 // should return transformed ast
 export function transformer(ast) {
-  
+  return walk(
+    ast, {
+        enter(node) {
+            if (node.type === 'VariableDeclaration') {
+                if (node.kind === 'let' || node.kind === 'const') {
+                    node.kind = 'var';
+                }
+            }
+        },
+    },
+  );
 }
