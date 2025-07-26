4a6737ecddd2695e1cda3270147a1f41b31ec3b2
CoinerLo <coiner4677@gmail.com>
2025-07-26 21:27:55 +0300

    1 iteration 5 task

--- tasks/project/webpack.config.ts
+++ tasks/project/webpack.config.ts
@@ -3,8 +3,68 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const templateContent = `
+<html>
+    <body>
+        <div id="root"></div>
+    </body>
+</html>
+`;
+
 const config: Configuration = {
-  
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve("./dist/webpack"),
+    },
+    devtool: false,
+    mode: isProduction ? "production" : "development",
+    target: ['web', 'es5'],
+    resolve: {
+        extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+        plugins: [
+            new TsconfigPathsPlugin({
+                
+            }),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(j|t)sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "swc-loader",
+                    options: {
+                        "jsc": {
+                            "parser": {
+                            "syntax": "typescript",
+                            "tsx": true
+                            },
+                            "transform": {
+                            "react": {
+                                "runtime": "automatic"
+                            }
+                            }
+                        },
+                        "module": {
+                            "type": "commonjs"
+                        }
+                    }
+                },
+            },
+            { test: /\.(html)$/, use: ['html-loader'] },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            templateContent,
+        }),
+    ],
+    experiments: {
+        css: true
+    }
 };
 
 export default config;
