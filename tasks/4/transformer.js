2602b9455e1a2fbca354bd709be3426a316fdcfd
CoinerLo <coiner4677@gmail.com>
2025-07-28 21:21:16 +0300

    update fix

--- tasks/4/transformer.js
+++ tasks/4/transformer.js
@@ -4,13 +4,13 @@ import { walk } from 'estree-walker'
 export function transformer(ast) {
   return walk(
     ast, {
-        enter(node) {
-            if (node.type === 'VariableDeclaration') {
-                if (node.kind === 'let' || node.kind === 'const') {
-                    node.kind = 'var';
-                }
-            }
-        },
+      enter(node) {
+        if (node.type === 'VariableDeclaration') {
+          if (node.kind === 'let' || node.kind === 'const') {
+            node.kind = 'var';
+          }
+        }
+      },
     },
   );
 }
