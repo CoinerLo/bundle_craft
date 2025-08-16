f7c89f8cfabe483dab9e8f68602bf58a2d2f8b3c
CoinerLo <coiner4677@gmail.com>
2025-08-16 13:00:15 +0300

    1 iteration 10 task

--- tasks/10/watch.js
+++ tasks/10/watch.js
@@ -1,9 +1,24 @@
+import fg from 'fast-glob';
+import { watch } from 'chokidar';
+
 const changedFilesLog = [];
 
 export function getLog() {
   return changedFilesLog;
 }
 
-export async function subscribe() {}
+let watcher;
 
-export function unsubscribe() {}
+export async function subscribe() {
+  const glob = await fg.glob('./src/**/*.js');
+  watcher = watch(glob).on('change', (path, stats) => {
+    changedFilesLog.push(path);
+    // console.log(stats, path);
+  });
+}
+
+export function unsubscribe() {
+  watcher
+    .close();
+    // .then(() => console.log('closed'));
+}
