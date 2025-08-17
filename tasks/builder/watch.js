c84b0ffe8d4fdf8041b30f9073ba2900752ede1c
CoinerLo <coiner4677@gmail.com>
2025-08-17 12:19:41 +0300

    2 iteration 10 task

--- tasks/builder/watch.js
+++ tasks/builder/watch.js
@@ -1,4 +1,6 @@
 import fs from "node:fs";
+import path from "node:path";
+import { watch } from 'chokidar';
 
 import { bundle } from "./bundle.js";
 
@@ -9,3 +11,13 @@ fs.mkdirSync("./dist", { recursive: true });
 fs.writeFileSync(`./dist/main.js`, output);
 
 // Реализуйте watch здесь
+
+export default function watch() {
+    const dir = path.dirname(entryPath);
+    console.log('Start watch');
+    watch(dir).on('change', (path, stats) => {
+        const newOutput = bundle(entryPath);
+        fs.writeFileSync(`./dist/main.js`, newOutput);
+    });
+}
+watch();
