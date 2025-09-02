f55d4fdd2d37cf083e42f03b21cad641c063f528
CoinerLo <coiner4677@gmail.com>
2025-09-02 19:20:13 +0300

    2 iteration 13 task

--- tasks/library/rollup.config.js
+++ tasks/library/rollup.config.js
@@ -1,29 +1,16 @@
 import typescript from "@rollup/plugin-typescript";
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: 'dist/index.mjs',
-      format: 'esm',
-      sourcemap: true
+      file: 'dist/index.js',
+      format: 'es',
     }
   ],
   plugins: [
-    resolve({
-      browser: false
-    }),
-    commonjs(),
     typescript({
       tsconfig: 'tsconfig.json'
     })
   ],
-//   external: ['fs', 'path', 'util']
 };
