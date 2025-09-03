2a2cdfd370457d19666e98e75a97d246fd3d3f55
CoinerLo <coiner4677@gmail.com>
2025-09-03 21:04:50 +0300

    3 iteration 13 task

--- tasks/library/rollup.config.js
+++ tasks/library/rollup.config.js
@@ -4,13 +4,16 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'lib/index.js',
       format: 'es',
     }
   ],
   plugins: [
     typescript({
-      tsconfig: 'tsconfig.json'
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'lib',
+      emitDeclarationOnly: true,
     })
   ],
 };
