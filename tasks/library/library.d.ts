35ed9d9bfe890ccee14d71669120c334d23d4c69
CoinerLo <coiner4677@gmail.com>
2025-09-13 11:00:22 +0300

    fix 13 task

---
+++ tasks/library/library.d.ts
@@ -0,0 +1,7 @@
+export declare const add: (a: number, b: number) => number;
+
+export declare const mul: (a: number, b: number) => number;
+
+export declare const sub: (a: number, b: number) => number;
+
+export { }
--- tasks/library/rollup.config.js
+++ tasks/library/rollup.config.js
@@ -1,12 +1,13 @@
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   output: {
     preserveModules: true,
     dir: "./lib",
+    format: "es",
   },
   plugins: [
-    typescript({ tsconfig: './tsconfig.json' }),
+    typescript({ tsconfig: "./tsconfig.json" }),
   ],
 };
