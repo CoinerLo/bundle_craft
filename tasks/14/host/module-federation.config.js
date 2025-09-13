492985a88ecaade6f3e34bb711660ce854a808a1
CoinerLo <coiner4677@gmail.com>
2025-09-13 11:13:17 +0300

    1 iteration 14 task

--- tasks/14/host/module-federation.config.js
+++ tasks/14/host/module-federation.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  
+  name: "host",
+  remotes: {
+    components: "",
+  },
+  shared: {
+    react: {
+        singleton: true,
+    },
+    "react-dom": {
+        singleton: true,
+    }
+  }
 };
--- tasks/14/host/webpack.config.js
+++ tasks/14/host/webpack.config.js
@@ -5,5 +5,43 @@ const {
 const mfConfig = require("./module-federation.config");
 
 module.exports = {
-  
+  entry: "./src/index.jsx",
+  mode: "development",
+  devtool: false,
+  output: {
+    publicPath: "http://localhost:3001/",
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new ModuleFederationPlugin(mfConfig),
+    new HtmlWebpackPlugin({
+      template: "./template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "swc-loader",
+          options: {
+            jsc: {
+              parser: {
+                syntax: "ecmascript",
+                tsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: "classic",
+                },
+              },
+            },
+          }
+        },
+      },
+    ],
+  },
 };
