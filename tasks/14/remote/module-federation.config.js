1f6868eae7a079594baa145b94712b67b28ea7e0
CoinerLo <coiner4677@gmail.com>
2025-09-13 12:13:10 +0300

    3 iteration 14 task

--- tasks/14/remote/module-federation.config.js
+++ tasks/14/remote/module-federation.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  
+name: "components",
+  exposes: {
+    ".": "./src/index.js",
+  },
+  shared: {
+    react: {
+        singleton: true,
+    },
+    "react-dom": {
+        singleton: true,
+    }
+  },
 };
--- tasks/14/remote/webpack.config.js
+++ tasks/14/remote/webpack.config.js
@@ -5,5 +5,50 @@ const {
 const mfConfig = require("./module-federation.config");
 
 module.exports = {
-  
+  entry: "./src/index.js",
+  mode: "development",
+  devtool: false,
+  output: {
+    publicPath: "http://localhost:3003/",
+  },
+  devServer: {
+    port: 3003,
+    headers: {
+      "access-control-allow-origin": "*",
+      "access-control-allow-methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new ModuleFederationPlugin(mfConfig),
+    new HtmlWebpackPlugin({
+      template: "./template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "swc-loader",
+          options: {
+            jsc: {
+              parser: {
+                syntax: "ecmascript",
+                jsx: true,
+              },
+              transform: {
+                react: {
+                  runtime: "classic",
+                },
+              },
+            },
+          }
+        },
+      },
+    ],
+  },
 };
