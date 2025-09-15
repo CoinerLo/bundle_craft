4978e6f45a7f15faf9cf6c10bd29922eb3962d0d
CoinerLo <coiner4677@gmail.com>
2025-09-15 21:29:38 +0300

    1 iteration 15 task

--- tasks/project/vite.config.ts
+++ tasks/project/vite.config.ts
@@ -1,5 +1,7 @@
+import { OutputBundle } from 'rollup';
 import { defineConfig } from 'vite';
 import tsconfigPaths from "vite-tsconfig-paths";
+const { createHash } = require('crypto');
 
 export default defineConfig({
     base: '/vite/',
@@ -8,6 +10,13 @@ export default defineConfig({
         outDir: 'dist/vite',
         assetsDir: '.',
         sourcemap: "hidden",
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`,
+            },
+        },
     },
     resolve: {
         alias: {
@@ -17,5 +26,42 @@ export default defineConfig({
             "store/*": "./src/store/*",
         },
     },
-    plugins: [tsconfigPaths()],
+    plugins: [tsconfigPaths(), pluginRenameFiles()],
 })
+
+function toBase64URL(buffer) {
+  return buffer
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+}
+
+function pluginRenameFiles() {
+    return {
+        name: 'rename-files-with-hash',
+        async generateBundle(_options, bundle: OutputBundle) {
+            for (const [fileName, chunk] of Object.entries(bundle)) {
+                if (chunk.type === 'chunk') {
+                    const code = chunk.code;
+
+                    // Считаем хеш от содержимого
+                    const hash = createHash('sha256').update(code).digest();
+                    const shortHash = toBase64URL(hash).slice(0, 8);
+
+                    // Получаем базовое имя без расширения
+                    const name = fileName.replace(/\.\w+$/, '').toLowerCase(); // -> 'index', 'main'
+
+                    // Определяем расширение
+                    const ext = fileName.split('.').pop(); // -> 'js'
+
+                    // Новое имя: [name]_[hash:8].[ext]
+                    const newName = `${name}_${shortHash}.${ext}`;
+
+                    // Переименовываем файл
+                    chunk.fileName = newName;
+                }
+            }
+        },
+    }
+};
