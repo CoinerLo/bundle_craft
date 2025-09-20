907e4eaecf146a2bc72e3c9a1943eef67247aa45
CoinerLo <coiner4677@gmail.com>
2025-09-20 15:52:53 +0300

    1 iteration 17 task

--- tasks/17/webpack.config.js
+++ tasks/17/webpack.config.js
@@ -3,7 +3,46 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const config = {
-  
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(import.meta.dirname, 'dist/webpack'),
+        filename: 'main.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.module\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: false,
+                            modules: {
+                                localIdentName: '[path][name]_[hash:base64:5]',
+                            }
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.css$/i,
+                exclude: /\.module\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body'
+        })
+    ]
 };
 
 export default config;
