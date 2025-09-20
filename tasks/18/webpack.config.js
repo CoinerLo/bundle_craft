5ae50af24a9ee971c12e5d3acd5bfb69e2d4f5b0
CoinerLo <coiner4677@gmail.com>
2025-09-20 15:24:49 +0300

    1 iteration 18 task

--- tasks/18/webpack.config.js
+++ tasks/18/webpack.config.js
@@ -2,5 +2,24 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import { GenerateSW } from "workbox-webpack-plugin";
 
 export default {
-  
+    entry: "./src/index.js",
+    mode: "development",
+    target: ['web', 'es5'],
+    resolve: {
+        extensions: [".js", ".json"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './template.html' }),
+        new GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true,
+            cleanupOutdatedCaches: true,
+            navigateFallback: '/template.html',
+        }),
+    ],
+    // module: {
+    //     rules: [
+    //         { test: /\.(html)$/, use: ['html-loader'] },
+    //     ],
+    // },
 };
