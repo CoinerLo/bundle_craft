bbd54fe9a99f9322ebf4793f4eff4544750bfddd
CoinerLo <coiner4677@gmail.com>
2025-09-20 16:08:57 +0300

    3 iteration 17 task

--- tasks/17/rollup.config.js
+++ tasks/17/rollup.config.js
@@ -4,5 +4,33 @@ import commonjs from "@rollup/plugin-commonjs";
 import html from "@rollup/plugin-html";
 
 export default {
-  
+    input: 'src/index.js',
+    output: {
+        file: 'dist/rollup/main.js',
+        format: 'iife',
+        sourcemap: false,
+    },
+    plugins: [
+        nodeResolve({
+            browser: true,
+            preferBuiltins: false,
+        }),
+        commonjs(),
+        postcss({
+            modules: {
+                generateScopedName: '[path][name]_[hash:base64:5]'
+            },
+            extract: true,
+            minimize: true
+        }),
+        html({
+            fileName: 'index.html',
+            title: 'Rollup CSS Modules demo',
+            template: ({ attributes, files, meta, publicPath, title }) => {
+                const css = (files.css || []).map(f => `<link rel="stylesheet" href="${f.fileName}">`).join('\n');
+                const js = (files.js || []).map(f => `<script src="${f.fileName}"></script>`).join('\n');
+                return `<!doctype html><html><head><meta charset="utf-8"><title>${title}</title>${css}</head><body><div id="root"></div>${js}</body></html>`;
+            }
+        }),
+    ]
 };
