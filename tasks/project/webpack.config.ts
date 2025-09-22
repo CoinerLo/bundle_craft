6509983a116cc823ac108377b530afa99f986d7a
CoinerLo <coiner4677@gmail.com>
2025-09-22 21:14:11 +0300

    1 fix iteration 15 task

--- tasks/project/webpack.config.ts
+++ tasks/project/webpack.config.ts
@@ -55,10 +55,7 @@ const config: Configuration = {
     output: {
         path: path.resolve("./dist/webpack"),
         publicPath: '/webpack/',
-        filename: (pathData) => {
-            const name = pathData.chunk?.name;
-            return `${name}_[contenthash:8].js`;
-        },
+        filename: '[name_[contenthash:8].js',
         chunkFilename: '[name]_[contenthash:8].js',
         assetModuleFilename: '[name]_[hash:8][ext]',
         clean: true,
