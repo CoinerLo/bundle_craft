7b8db3d9c1aeb842df75a4e2a9ac76b3eebc742e
CoinerLo <coiner4677@gmail.com>
2025-09-28 18:32:39 +0300

    fix iteration builder webpack

--- tasks/builder/hash.js
+++ tasks/builder/hash.js
@@ -1,9 +1,15 @@
 import fs from "node:fs";
-
+import crypto from 'node:crypto';
 import { bundle } from "./bundle.js";
 
 const output = bundle(process.argv[2]);
 
 fs.mkdirSync("./dist", { recursive: true });
-// TODO: hash from content in filename
-fs.writeFileSync(`./dist/main.js`, output);
+
+function hashString(str, algo = 'md5') {
+    return crypto.createHash(algo).update(str).digest('base64');
+}
+
+const hash = hashString(output)
+
+fs.writeFileSync(`./dist/main.${hash.slice(0, 6)}.js`, output);
