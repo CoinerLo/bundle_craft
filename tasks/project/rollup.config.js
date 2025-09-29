9a2634d5935242b25a758de289ad53ad27a679b2
CoinerLo <coiner4677@gmail.com>
2025-09-29 21:38:17 +0300

    1 fix iteration project rollup

--- tasks/project/rollup.config.js
+++ tasks/project/rollup.config.js
@@ -3,22 +3,21 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import html from "@rollup/plugin-html";
 import typescript from "@rollup/plugin-typescript";
-import { createHash } from 'crypto';
 
-const htmlTemplate = ({ files }) => {
+const htmlTemplate = ({ files, title, publicPath }) => {
     const scripts = (files.js || [])
-        .map(file => `<script type="module" src="/rollup/${file.name}.js" nonce="{{NONCE_VALUE}}"></script>`)
-        .join('\n  ');
+        .map(({ fileName }) => `<script type="module" src="${publicPath}${fileName}" nonce="{{NONCE_VALUE}}"></script>`)
+        .join('\n ');
 
     const styles = (files.css || [])
-        .map(file => `<link rel="stylesheet" href="/rollup/${file.name}" nonce="{{NONCE_VALUE}}">`)
-        .join('\n  ');
+        .map(({ fileName }) => `<link rel="stylesheet" href="${publicPath}${fileName}" nonce="{{NONCE_VALUE}}">`)
+        .join('\n ');
     return `
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>Document</title>
+        <title>${title}</title>
         <meta property="csp-nonce" nonce="{{NONCE_VALUE}}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         ${styles}
@@ -35,9 +34,9 @@ export default {
     output: {
         dir: "dist/rollup",
         format: "esm",
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+        entryFileNames: `[name]_[hash:8].js`,
+        chunkFileNames: `[name]_[hash:8].js`,
+        assetFileNames: `[name]_[hash:8].[ext]`,
         sourcemap: 'hidden',
     },
     plugins: [
@@ -46,9 +45,7 @@ export default {
         }),
         typescript(),
         commonjs({ transformMixedEsModules: true, extensions: [".js", ".ts", ".tsx", ".jsx", ".json"] }),
-        css({
-            output: 'style.css', // имя выходного CSS
-        }),
+        css(),
         html({
             template: htmlTemplate,
             publicPath: '/rollup/',
@@ -61,43 +58,5 @@ export default {
                     .replace(/\bprocess\.env\b/g, '({ NODE_ENV: "production" })');
             }
         },
-        // pluginRenameFiles(),
     ],
-};
-
-function toBase64URL(buffer) {
-    return buffer
-        .toString('base64')
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
-
-function pluginRenameFiles() {
-    return {
-        name: 'rename-files-with-hash',
-        async generateBundle(options, bundle) {
-            for (const [fileName, chunk] of Object.entries(bundle)) {
-                if (chunk.type === 'chunk') {
-                    const code = chunk.code;
-
-                    // Считаем хеш от содержимого
-                    const hash = createHash('sha256').update(code).digest();
-                    const shortHash = toBase64URL(hash).slice(0, 8);
-
-                    // Получаем базовое имя без расширения
-                    const name = fileName.replace(/\.\w+$/, '').toLowerCase(); // -> 'index', 'main'
-
-                    // Определяем расширение
-                    const ext = fileName.split('.').pop(); // -> 'js'
-
-                    // Новое имя: [name]_[hash:8].[ext]
-                    const newName = `${name}_${shortHash}.${ext}`;
-
-                    // Переименовываем файл
-                    chunk.fileName = newName;
-                }
-            }
-        },
-    }
 };
